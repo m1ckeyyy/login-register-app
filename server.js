45d64cd611e3fa23487ae2d4098667b60977db8f
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2023-01-06 17:24:20 +0100

    added register feature

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, "js")));
-app.use(express.static("public"));
 
 app.get("/", (request, response) => {
 	response.sendFile("index.html", { root: __dirname });
 });
-app.get("/script.js", (request, response) => {
+app.get("/src/login-script.js", (request, response) => {
 	// Adding script.js to the server like this because of problem with file type or directory
 	response.type("application/javascript");
-	response.sendFile("script.js", { root: __dirname });
+	response.sendFile("src/login-script.js", { root: __dirname });
 });
 
 app.get("/login", (request, response) => {
@@ -28,8 +26,32 @@ app.get("/login", (request, response) => {
 	// }
 });
 app.post("/login", (request, response) => {
-	//
-	console.log(request.body.username, request.body.password);
+	console.log("logged in: ", request.body.username, request.body.password);
+	//now save and check in database
+});
+// register
+app.get("/src/register-script.js", (request, response) => {
+	// Adding script.js to the server like this because of problem with file type or directory
+	response.type("application/javascript");
+	response.sendFile("src/register-script.js", { root: __dirname });
+});
+
+app.get("/register", (request, response) => {
+	response.sendFile("register.html", { root: __dirname });
+
+	// const username = request.query.username;
+	// const password = request.query.password;
+	// console.log("u", username);
+	// if (username === "admin" && password === "password") {
+	// 	// If the username and password are correct, send a success message
+	// 	response.send("Login successful");
+	// } else {
+	// 	// If the username and password are incorrect, send an error message
+	// 	response.status(401).send("Error: Invalid username or password");
+	// }
+});
+app.post("/register", (request, response) => {
+	console.log("registered: ", request.body.username, request.body.password);
 	//now save and check in database
 });
 
---
+++ src/register-script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("login-form");
+form.addEventListener("submit", (event) => {
+	event.preventDefault(); // Prevent the form from reloading the page
+	const formData = new FormData(form); // Get the form data
+	const data = {}; // Convert the form data to a JavaScript object
+	for (const [key, value] of formData.entries()) {
+		data[key] = value;
+	}
+	console.log(data);
+	document.querySelector("#username").value = "";
+	document.querySelector("#password").value = "";
+
+	// Send a POST request to the server with the form data
+	fetch("/register", {
+		method: "POST",
+		body: JSON.stringify(data),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => response.json())
+		.then((result) => {
+			// Do something with the result
+		})
+		.catch((err) => {
+			console.error("error: ", err);
+		});
+});
