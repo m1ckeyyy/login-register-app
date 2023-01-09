6af1b95377c349986dfeebbd1afdd82d8f5ee721
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-01-09 14:47:18 +0000

    hashed passwords

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const session = require('express-session')
+const session = require("express-session");
 const bcrypt = require("bcrypt");
 
 // const session = require("express-session");
@@ -11,13 +11,13 @@ const uri = config.mongoURI;
 app.use(express.json());
 mongoose.set("strictQuery", false); //supress warning
 
-app.use(
-	session({
-		secret: "my-sgfrdesgfr6grdterdgrtegerggre432ecret", // a secret key to sign the session ID cookie
-		resave: false, // don't save the session if it wasn't modified
-		saveUninitialized: false, // don't create a session until something is stored
-	})
-);
+// app.use(
+// 	session({
+// 		secret: "my-sgfrdesgfr6grdterdgrtegerggre432ecret", // a secret key to sign the session ID cookie
+// 		resave: false, // don't save the session if it wasn't modified
+// 		saveUninitialized: false, // don't create a session until something is stored
+// 	})
+// );
 
 mongoose
   .connect(uri)
@@ -68,22 +68,40 @@ app.post("/login", (request, response) => {
     request.body.password,
     "..."
   );
-  //now save and check in database
-
-  const username = request.body.username;
-  const password = request.body.password;
-
-  User.findOne({ username, password }, (err, user) => {
-    if (err) {
-      response.status(500).send("Error: " + err);
-      console.log("error with MongoDB");
-    } else if (!user) {
-      response.status(401).send("Error: Invalid username or password");
-      console.log("user not found!!!");
-    } else {
-      console.log("Login successful!!");
-    }
-  });
+  try {
+    const username = request.body.username;
+    const password = request.body.password;
+    //first find username
+    User.findOne({ username }).then((user) => {
+      if (!user) {
+        // console.log('user nono')
+        return response.status(404).send("User not found");
+      }
+      bcrypt.compare(password, user.password).then((isMatching) => {
+        if (isMatching) {
+          response.send("Logged in successfully");
+          // console.log("logged in");
+        } else {
+          response.status(401).send("Incorrect password");
+          // console.log("incorrect password");
+        }
+      });
+    });
+  } catch (err) {
+    response.status(500).send("An error occured");
+    console.log(err);
+  }
+  // User.findOne({ username, password }, (err, user) => {
+  //   if (err) {
+  //     response.status(500).send("Error: " + err);
+  //     console.log("error with MongoDB");
+  //   } else if (!user) {
+  //     response.status(401).send("Error: Invalid username or password");
+  //     console.log("user not found!!!");
+  //   } else {
+  //     console.log("Login successful!!");
+  //   }
+  // });
 });
 
 app.listen(port, () => {
