a65748b045b5b630827a9bb7f98a1d6d41d79deb
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-01-19 14:21:06 +0000

    frontend server connection established

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,46 +1,85 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { blue, deepPurple } from '@mui/material/colors';
-import LoginIcon from '@mui/icons-material/Login';
-import CardMedia from '@mui/material/CardMedia';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+// import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { blue, deepPurple } from "@mui/material/colors";
+import LoginIcon from "@mui/icons-material/Login";
+// import CardMedia from "@mui/material/CardMedia";
+// import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-import '../index.css'; //fonts
+import "../index.css"; //fonts
+import { Form } from "react-router-dom";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" target="_blank" href="https://github.com/m1ckeyyy">
         m1ckeyyy
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  // const onSubmit = (data) => console.log(data);
+  // console.log(errors);
+  const submitHandler = (data) => {
+    // console.log("data:", JSON.stringify(data));
+    fetch("https://fnvzol-8080.preview.csb.app/loginx", {
+      mode: "cors",
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      // headers: { "Content Type": "application/json" },
+    })
+      .then((res) => res.status)
+      .then((response) => {
+        if (response == 200) {
+          console.log("successful login");
+        } else {
+          console.log("Error: ", response.message);
+        }
+      })
+      .catch((error) => console.error("Error2: ", error));
+  };
+
   const paperStyle = {
-    padding: '20px 20px 25px 20px',
+    padding: "20px 20px 25px 20px",
     // height: '40vh',
-    width: 280,
-    margin: 'auto auto',
+    width: 335,
+    margin: "18vh auto",
   };
   const theme = createTheme({
     typography: {
-      fontFamily: 'Roboto Mono',
+      fontFamily: "Roboto Mono",
       h1: {
         fontSize: 32,
         fontWeightLight: 200,
@@ -50,18 +89,27 @@ const Login = () => {
       },
     },
   });
-  const signInStyle = { margin: '20px 0 ' };
-  const btnStyle = { margin: '5px 0 15px 0' };
+  const signInStyle = { margin: "20px 0 " };
+  const btnStyle = { margin: "5px 0 15px 0" };
   const inputStyle = {
     // padding: '16.5px 14px',
-    margin: '5px 0 0 0',
+    margin: "10px 0 0 0",
   };
-  const rememberMeStyle = { marginTop: '5px', fontSize: '12px', padding: '0px' };
-  const forgotPassStyle = { margin: '0 0 5px 0' };
-  const avatarStyle = { backgroundColor: '#a6c660' };
+  const rememberMeStyle = {
+    marginTop: "5px",
+    fontSize: "12px",
+    padding: "0px",
+  };
+  const forgotPassStyle = { margin: "0 0 5px 0" };
+  const avatarStyle = { backgroundColor: "#a6c660" };
   return (
     <ThemeProvider theme={theme}>
-      <Grid container direction="column" alignItems="center" style={{ height: '100vh' }}>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        style={{ height: "100vh" }}
+      >
         <Paper elevation={10} style={paperStyle}>
           <Grid align="center">
             <Avatar style={avatarStyle}>
@@ -71,20 +119,66 @@ const Login = () => {
               Sign In
             </Typography>
           </Grid>
-          <TextField label="Username" placeholder="Enter Username" style={inputStyle} fullWidth required />
-          <TextField label="Password" placeholder="Enter Password" style={inputStyle} type="password" fullWidth required />
-          <FormControlLabel control={<Checkbox name="checkedB" color="primary" />} label="Remember me" style={rememberMeStyle}></FormControlLabel>
-          <Button type="submit" color="primary" variant="contained" style={btnStyle} fullWidth>
-            Sign In
-          </Button>
+          <form onSubmit={handleSubmit(submitHandler)}>
+            <TextField
+              label="Username"
+              placeholder="Enter Username"
+              style={inputStyle}
+              fullWidth
+              {...register("username", {
+                required: "Username is required.",
+                minLength: {
+                  value: 5,
+                  message: "Username needs to be at least 5 characters",
+                },
+              })}
+              error={Boolean(errors.username)}
+              helperText={errors.username?.message}
+            />
+            <TextField
+              label="Password"
+              placeholder="Enter Password"
+              style={inputStyle}
+              type="password"
+              fullWidth
+              {...register("password", {
+                required: "Password is required.",
+                pattern: {
+                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$/,
+                  message:
+                    "Password must be at least 8 characters, contain at least one uppercase letter, one lowercase letter and one number",
+                },
+              })}
+              error={Boolean(errors.password)}
+              helperText={errors.password?.message}
+            />
+            <FormControlLabel
+              control={<Checkbox name="checkedB" color="primary" />}
+              label="Remember me"
+            ></FormControlLabel>
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              style={btnStyle}
+              fullWidth
+            >
+              Sign In
+            </Button>
+          </form>
           <Typography variant="h2" style={forgotPassStyle}>
-            <Link href="#">Forgot password?</Link>
+            <Link href="#" target="_blank">
+              Forgot password?
+            </Link>
           </Typography>
           <Typography variant="h2">
-            Don't have an account? <Link href="#">Sign Up</Link>
+            Don't have an account?{" "}
+            <Link href="#" target="_blank">
+              Sign Up
+            </Link>
           </Typography>
         </Paper>
-        <Copyright sx={{ mt: 3, position: 'absolute' }} />
+        <Copyright sx={{ mt: 3, position: "absolute" }} />
       </Grid>
     </ThemeProvider>
   );
--- server/server.js
+++ server/server.js
@@ -11,6 +11,12 @@ const cors = require("cors");
 const config = require("./config");
 const uri = config.mongoURI;
 app.use(cors());
+// app.use(
+//   cors({
+//     origin: "https://fnvzol-5174.preview.csb.app/*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 app.use(require("connect-flash")());
 // app.use(function (req, res, next) {
 //   res.locals.messages = require("express-messages")(req, res);
@@ -20,129 +26,138 @@ app.use(require("connect-flash")());
 app.use(cookieParser());
 app.use(express.json());
 app.use(
-	session({
-		secret: "my-sgfrdesgfr6grdterdgrtegerggre432ecret", // a secret key to sign the session ID cookie
-		cookie: { maxAge: Infinity },
-		resave: false, // don't save the session if it wasn't modified
-		saveUninitialized: true, // don't create a session until something is stored
-	})
+  session({
+    secret: "my-sgfrdesgfr6grdterdgrtegerggre432ecret", // a secret key to sign the session ID cookie
+    cookie: { maxAge: Infinity },
+    resave: false, // don't save the session if it wasn't modified
+    saveUninitialized: true, // don't create a session until something is stored
+  })
 );
 mongoose.set("strictQuery", false); //supress warning
 mongoose
-	.connect(uri)
-	.then(() => console.log("Successfully connected to MongoDB"))
-	.catch((error) => console.error(error));
+  .connect(uri)
+  .then(() => console.log("Successfully connected to MongoDB"))
+  .catch((error) => console.error(error));
 
 const User = require("./User");
 
 app.get("/", authenticate, (request, response) => {
-	//   const messages = request.flash("success");
-	response.sendFile("index.html", { root: __dirname });
-	//   res.write(`<script> var messages = ${JSON.stringify(messages)} </script>`);
+  //   const messages = request.flash("success");
+  response.sendFile("index.html", { root: __dirname });
+  //   res.write(`<script> var messages = ${JSON.stringify(messages)} </script>`);
 });
 
 app.get(["/register", "/login"], notAuthenticated, (request, response) => {
-	response.sendFile(`${request.path.substring(1)}.html`, { root: __dirname });
+  response.sendFile(`${request.path.substring(1)}.html`, { root: __dirname });
 });
 
 app.get(
-	["/src/register-script.js", "/src/login-script.js"],
-	(request, response) => {
-		response.type("application/javascript");
-		response.sendFile(request.path, { root: __dirname });
-	}
+  ["/src/register-script.js", "/src/login-script.js"],
+  (request, response) => {
+    response.type("application/javascript");
+    response.sendFile(request.path, { root: __dirname });
+  }
 );
 
 app.post("/register", (request, response) => {
-	const { username, password } = request.body;
-	const newUser = new User({ username, password });
+  const { username, password } = request.body;
+  const newUser = new User({ username, password });
 
-	console.log("server.js,/register,newUser: ", newUser);
-	const validationError = newUser.validateSync({ username, password });
-	if (validationError) {
-		const { message } = validationError.errors.password;
-		console.log(message);
-		return response.status(400).send({ error: message });
-	}
-	newUser.save((error) => {
-		if (error) {
-			response.status(500).send("Error saving user to database");
-		} else {
-			console.log("user yes");
-			//   request.flash("success", "You are now registered and can log in");
-			response.status(200).json({ redirect: "/login" });
-		}
-	});
+  console.log("server.js,/register,newUser: ", newUser);
+  const validationError = newUser.validateSync({ username, password });
+  if (validationError) {
+    const { message } = validationError.errors.password;
+    console.log(message);
+    return response.status(400).send({ error: message });
+  }
+  newUser.save((error) => {
+    if (error) {
+      response.status(500).send("Error saving user to database");
+    } else {
+      console.log("user yes");
+      //   request.flash("success", "You are now registered and can log in");
+      response.status(200).json({ redirect: "/login" });
+    }
+  });
 });
 
 app.post("/login", (request, response) => {
-	console.log(
-		"logging in: ",
-		request.body.username,
-		request.body.password,
-		"..."
-	);
-	let username, password;
-	try {
-		username = request.body.username;
-		password = request.body.password;
+  console.log(
+    "logging in: ",
+    request.body.username,
+    request.body.password,
+    "..."
+  );
+  let username, password;
+  try {
+    username = request.body.username;
+    password = request.body.password;
 
-		User.findOne({ username }).then((user) => {
-			if (!user) {
-				return response.status(404).send("User not found");
-			}
-			bcrypt.compare(password, user.password).then((isMatching) => {
-				if (isMatching) {
-					password = user.password;
-					request.session.authenticated = true;
-					request.session.user = {
-						username,
-						password,
-					};
-					//add a flash message, to append a success login message in the navigation bar
-					response.status(200).json({ redirect: "/" });
-				} else {
-					response.status(401).send("Incorrect password");
-				}
-			});
-		});
-	} catch (err) {
-		response.status(500).send("An error occured");
-		console.log(err);
-	}
+    User.findOne({ username }).then((user) => {
+      if (!user) {
+        return response.status(404).send("User not found");
+      }
+      bcrypt.compare(password, user.password).then((isMatching) => {
+        if (isMatching) {
+          password = user.password;
+          request.session.authenticated = true;
+          request.session.user = {
+            username,
+            password,
+          };
+          //add a flash message, to append a success login message in the navigation bar
+          response.status(200).json({ redirect: "/" });
+        } else {
+          response.status(401).send("Incorrect password");
+        }
+      });
+    });
+  } catch (err) {
+    response.status(500).send("An error occured");
+    console.log(err);
+  }
 });
 
 app.get("/logout", (request, response) => {
-	request.session.destroy(function (err) {
-		if (err) {
-			console.log("logout error", err);
-		} else {
-			response.clearCookie("connect.sid");
-			response.redirect("/login");
-		}
-	});
+  request.session.destroy(function (err) {
+    if (err) {
+      console.log("logout error", err);
+    } else {
+      response.clearCookie("connect.sid");
+      response.redirect("/login");
+    }
+  });
 });
 app.get("/test-connection", (request, response) => {
-	response.setHeader("Content-Type", "application/json");
-	response.send({ siema: ["eniu", "enku", "enieczku"] });
+  response.setHeader("Content-Type", "application/json");
+  response.send({ siema: ["eniu", "enku", "enieczku"] });
 });
 app.listen(port, () => {
-	console.log("Server listening on port 8080");
+  console.log("Server listening on port 8080");
 });
 
+app.post("/loginx", (req, res) => {
+  console.log("Bbb", req.body);
+  res.status(200).send("some text");
+});
+app.get("/loginx", (req, res) => {
+  //   console.log("Aaa");
+  //   res.status(200).send("Current yes");
+  res.send("hello321312");
+});
 function authenticate(req, res, next) {
-	if (req.session.authenticated) {
-		console.log("AUTHENTICATed");
-		next();
-	} else {
-		console.log("NOT AUTHenticated");
-		res.redirect("/login");
-	}
+  if (req.session.authenticated) {
+    console.log("AUTHENTICATed");
+    next();
+  } else {
+    console.log("NOT AUTHenticated");
+    res.redirect("/login");
+  }
 }
 function notAuthenticated(req, res, next) {
-	if (!req.session.authenticated) {
-		next();
-	} else {
-		res.redirect("/");
-	}
+  if (!req.session.authenticated) {
+    next();
+  } else {
+    res.redirect("/");
+  }
 }
